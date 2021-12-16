94f739cf7956fb9b494595adcbd8bcb78edaddf7
Opeyemi Gbadamosi <gbadamosiopeyemi99@gmail.com>
2021-12-16 22:38:49 +0100

    Implemented before function

--- cypress/integration/test/login.spec.js
+++ cypress/integration/test/login.spec.js
@@ -1,23 +1,43 @@
 /// <reference types="cypress" />
 
 describe('Test Konga Login', ()=>{
-    it('Launch the site', ()=>{
-        cy.openSite()
-    })
-    it('Click the Sign in Icon', ()=>{
+    before(()=>{
+        //run before everything else
         cy.fixture('elementMapper').then((em)=>{
-        cy.clickAnElement(em.signInIcon)
+            cy.openSite()
+            cy.clickAnElement(em.signInIcon)
+            cy.typeAText(em.emailField, em.emailText)
+            cy.typeAText(em.passwordField, em.passwordText)
+            cy.clickAnElement(em.loginBtn)
         })
     })
-    it('Insert Credentials', ()=>{
+   
+    
+    it('Search for phone', ()=>{
         cy.fixture('elementMapper').then((em)=>{
-        cy.typeAText(em.emailField, em.emailText)
-        cy.typeAText(em.passwordField, em.passwordText)
+        cy.clickAnElement(em.searchField)
+        cy.typeAText(em.searchField, em.searchText)
+        cy.clickAnElement(em.searchIcon)
         })
     })
-    it('Click Sign In Button', ()=>{
+    it('Select the 4th Item', ()=>{
         cy.fixture('elementMapper').then((em)=>{
-        cy.clickAnElement(em.loginBtn)
+        cy.clickAnElement(em.phoneItem)
+        cy.clickAnElement(em.buyBtn)
+        })
+    })
+    it('Increment the Item and proceed to checkout', ()=>{
+        cy.fixture('elementMapper').then((em)=>{
+       // cy.clickAnElement(em.incrementBtn)
+       //cy.clickAnElement(em.incrementBtn)
+        cy.clickAnElement(em.contToCheckout)
+        
+        })
+    })
+    it('Place Order', ()=>{
+        cy.fixture('elementMapper').then((em)=>{
+        cy.clickAnElement(em.payOnDelievry)
+        cy.clickAnElement(em.placeOrderBtn)
         })
     })
 })
